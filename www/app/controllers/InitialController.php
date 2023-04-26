8073c191ec3cfcc4506142d7b814b0580ce2695d
Guilherme-fagundes <guilhermekfagundes259@gmail.com>
2023-04-26 01:24:10 -0300

    Add composer

---
+++ www/app/controllers/InitialController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\controllers;
+
+use app\controllers;
+
+
+class InitialController
+{
+    public function __construct()
+    {
+        phpinfo();
+        
+    }
+
+}
--- www/index.php
+++ www/index.php
@@ -1,3 +1,15 @@
 <?php
 
-phpinfo();
+use App\controllers\InitialController;
+
+require __DIR__. '/vendor/autoload.php';
+$std = new stdClass;
+
+var_dump($std);
+$init = new InitialController;
+
+
+
+
+
+
